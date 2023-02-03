eec706843b660e4be18d88fa4e897424fc0a090f
Njonge9 <frednjonge9@gmail.com>
2023-02-03 16:26:24 +0300

    Setup bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
   validates :email, uniqueness: true
   validates :email, :format => /@/
   validates :password_digest, presence: true
+
+  # Adds the following validation
+  # 1.Password must be present when creating.
+  # 2.Password length must be or equal to 72 bytes
+  # 3.confirmation of password using password_confirmation
+  has_secure_password
 end
